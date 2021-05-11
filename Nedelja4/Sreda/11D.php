4e9f147346107a8859f2bd20d0df0c00a90c76a3
Nidzoni8 <prokicanikola@gmail.com>
2021-05-11 22:28:24 +0200

    novi domaci

---
+++ Nedelja4/Sreda/11D.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+        $a = 200;
+        $b = 80;
+        $c = $a + $b;
+        $d = $a - $b;
+        $e = $a * $b;
+        $f = $a / $b;
+        echo $c. " Zbir <br/>";
+        echo $d. " Razlika <br/>";
+        echo $e. " Proizvod <br/>";
+        echo $f. " Kolicnik <br/>";
+    ?>
+<div style="margin-top:50px;">
+    <?php 
+        $a = 6;
+        $b = 5;
+        $c = 2;
+        $d = $a + $b + $c;
+        echo "Zbir brojeva $a, $b, i $c je $d."
+    ?>
+    </div>
+</body>
+</html>
