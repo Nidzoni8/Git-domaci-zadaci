5828de290ce300b7e2ec1044f07acac182aa0425
Nidzoni8 <prokicanikola@gmail.com>
2021-05-25 09:54:36 +0200

    domaci 18

---
+++ Nedelja5/Nedelja/18D/zadatak1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Ispisati brojeve od 20 do 1 preko while ciklusa.</p>
+
+    <?php
+        $a = 1;
+        while($a <= 20){
+            echo "".$a++."<br>";
+        }
+    ?>
+
+
+    //ne umem da odradim ostala 2 zadatka//
+</body>
+</html>
